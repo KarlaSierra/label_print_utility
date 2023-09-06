07e2c8b8413de40f7c195a34b47e1a1d9d852232
KarlaSierra <93312551+KarlaSierra@users.noreply.github.com>
2023-09-06 11:54:52 -0600

    Update basededatos.sql

--- basededatos.sql
+++ basededatos.sql
@@ -1,14 +1,11 @@
-drop database label_print_utility;
 create database label_print_utility;
 use label_print_utility;
 
-select * from ticket_gavilla;
-
-Create table ticket_gavilla (
+create table ticket_gavilla (
     id int primary key not null auto_increment, 
-    n_paca varchar (30),
-    clase varchar (50),
-	variedad varchar (50),
+    n_paca varchar(15),
+    clase varchar(30),
+    variedad varchar(50),
     hojas_gavillas int,
     gavillas_funda int,
     hojas_fundas int,
@@ -17,16 +14,21 @@ Create table ticket_gavilla (
     cant_gavillas_paca int,
     prom_hojas_paca float,
     cant_fundas_paca int,
-	tamano varchar(4), 
+    tamano varchar(10), 
     n_tickets int,
     fecha_elaboracion date,
-    sobrante int
+    sobrante int,
+    muestra_gavillas_sumatra int,
+    gavilla_funda_sumatra int,  
+    peso_funda_sumatra float,   
+    cant_fundas_paca_sumatra int,  
+    cant_gavillas_paca_sumatra float 
 );
 
-CREATE TABLE users (
-    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    user VARCHAR(50),
-    name VARCHAR(50),
-    password LONGTEXT,
-    rol VARCHAR(50)
+Create table users (
+    id int primary key not null auto_increment,
+    user varchar(30),
+    name varchar(50),
+    password longtext,
+    rol varchar(50)
 );
